b75719f420c4423d705d895c18fae1cfa680310e
Ivan Cardenas <ivanc@MacBook-Pro.local>
2023-05-22 19:34:17 -0300

    pop elements of queue

--- main.cpp
+++ main.cpp
@@ -7,9 +7,34 @@ struct Nodo {
 };
 
 void agregarNodo(Nodo *&, int );
+void sacarNodo(Nodo *&, int &);
 
 int main(){
     Nodo* pila = NULL;
+    int dato;
+
+    cout << "Digite un numero: ";
+    cin >> dato;
+    agregarNodo(pila, dato);
+
+    cout << "Digite un numero: ";
+    cin >> dato;
+    agregarNodo(pila, dato);
+
+    cout << "Digite un numero: ";
+    cin >> dato;
+    agregarNodo(pila, dato);
+
+    cout << "Sacando los elementos de la pila:" << endl;
+
+    while(pila != NULL) {
+        sacarNodo(pila, dato);
+        if(pila != NULL){
+            cout << dato << " , ";
+        } else {
+            cout << dato << "." << endl;
+        }
+    }
 
     return 0;
 }
@@ -19,4 +44,11 @@ void agregarNodo(Nodo* &pila , int n){
     newNode->dato = n;
     newNode->siguiente = pila;
     pila = newNode;
+}
+
+void sacarNodo(Nodo *&pila, int &n) {
+    Nodo* aux = pila;
+    n = aux->dato;
+    pila = aux->siguiente;
+    delete aux;
 }
